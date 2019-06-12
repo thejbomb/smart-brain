06fe6a469c128f7f1173558262cfd91e6e118318
Jun <junkun007@aim.com>
2019-06-11 21:57:11 -0400

    altered background

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './App.css';
 const particlesOptions = {
   particles: {
     number: {
-      value: 40,
+      value: 70,
       density: {
         enable: true,
         value_area: 800
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+const onInvalidInput = {
+  name: '',
+  email: '',
+  password: '',
+  invalid: true,
+}
+
 class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       email: '',
       password: '',
-      name: ''
+      name: '',
+      invalid: false,
     }
   }
 
@@ -23,13 +31,14 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://floating-plains-22616.herokuapp.com/register', {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-        name: this.state.name,
+    if (this.state.email.includes('@') && this.state.email.includes('.')) {
+      fetch('https://floating-plains-22616.herokuapp.com/register', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+          name: this.state.name,
       })
     })
     .then(response => response.json())
@@ -40,57 +49,71 @@ class Register extends React.Component {
           this.props.onRouteChange('home');
         }
       })
+    } else {
+      this.setState(onInvalidInput);
+      console.log('error')
+    }
   }
 
   render() {
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+      <div>
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+          <main className="pa4 black-80">
+            <div className="measure">
+              <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className="f1 fw6 ph0 mh0">Register</legend>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={this.onNameChange}
+                  />
+                </div>
+                <div className="mt3">
+                  <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                  <input
+                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="email"
+                    name="email-address"
+                    id="email-address"
+                    onChange={this.onEmailChange}
+                  />
+                </div>
+                <div className="mv3">
+                  <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                  <input
+                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                    type="password"
+                    name="password"
+                    id="password"
+                    onChange={this.onPasswordChange}
+                  />
+                </div>
+              </fieldset>
+              <div className="">
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="text"
-                  name="name"
-                  id="name"
-                  onChange={this.onNameChange}
+                  onClick={this.onSubmitSignIn}
+                  className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                  type="submit"
+                  value="Register"
                 />
               </div>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  onChange={this.onEmailChange}
-                />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
-                  type="password"
-                  name="password"
-                  id="password"
-                  onChange={this.onPasswordChange}
-                />
-              </div>
-            </fieldset>
-            <div className="">
-              <input
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit"
-                value="Register"
-              />
             </div>
-          </div>
-        </main>
-      </article>
+          </main>
+        </article>
+        { this.state.Invalid === true ?
+        <div className="pa2 f4 dark-red">
+          <p>
+            Invalid E-mail. <br/>Please use a proper E-mail.
+          </p>
+        </div>
+        : <div></div>
+        }
+      </div>
     );
   }
 }
