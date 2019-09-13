8f534e9da0959bbe5f5231820e3b1f3d54205566
Jun <junkun007@aim.com>
2019-09-12 22:07:47 -0400

    added multi recognition

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const initialState = {
   input: '',
   previousInput: '',
   imageUrl: '',
-  box: {},
+  boxes: [],
   route: 'signin',
   isSignedIn: false,
   invalid: false,
@@ -55,21 +55,23 @@ class App extends Component {
     }})
   }
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+  calculateFaceLocations = (data) => {
+    return data.outputs[0].data.regions.map(face => {
+      const clarifaiFace = face.region_info.bounding_box;
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - (clarifaiFace.right_col * width),
+        bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+    });
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+  displayFaceBoxes = (boxes) => {
+    this.setState({boxes: boxes});
   }
 
   onInputChange = (event) => {
@@ -114,7 +116,7 @@ class App extends Component {
             .catch(console.log)
 
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBoxes(this.calculateFaceLocations(response))
       })
       .catch(err => console.log(err));
 
@@ -135,7 +137,7 @@ class App extends Component {
   }
 
   render() {
-    const { isSignedIn, imageUrl, route, box, invalid } = this.state;
+    const { isSignedIn, imageUrl, route, boxes, invalid } = this.state;
     return (
       <div className="App">
          <Particles className='particles'
@@ -161,10 +163,10 @@ class App extends Component {
                     <p>
                       Invalid input. <br/>Please use a jpeg/png url or use a different url.
                     </p>
-                    <FaceRecognition box={box} imageUrl={imageUrl} />
+                    <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
                   </div>
                   : <div>
-                      <FaceRecognition box={box} imageUrl={imageUrl} />
+                      <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
                     </div>
                   }
               </div>
